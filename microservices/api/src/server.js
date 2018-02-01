cab558085dfc8cb3f25aa709023853aa53d837bd
manu0309 <guptamanu530@gmail.com>
2018-02-01 22:50:51 +0530

    file added

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -1,24 +1,271 @@
-var express = require('express');
-var app = express();
-var request = require('request');
-var router = express.Router();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-require('request-debug')(request);
+		var express = require('express');
+		var request = require('request');
+		var path = require('path');
+		var bodyParser = require('body-parser');
+		var fetchAction =  require('fetch');
 
-var hasuraExamplesRouter = require('./hasuraExamples');
+		var app = express();
+		app.use(bodyParser.urlencoded({extended: true}));
+		
+		app.post('/signpoint', function(req, res){
 
-var server = require('http').Server(app);
+		var auth_token;
+		var hasura_id;
+		var gender;
+		var dob;
+		var firstname;
+		var lastname;
+		var username;
+		var password;
 
-router.use(morgan('dev'));
+		username = req.body.email;
+	    password = req.body.password;
+		firstname = req.body.firstname;
+	    lastname = req.body.lastname;
+		dob = req.body.dob;
+		gender = req.body.gender;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
-app.use('/', hasuraExamplesRouter);
+		
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+		var url = "https://auth.adulthood94.hasura-app.io/v1/signup";
+
+		var requestOptions = {
+		    "method": "POST",
+		    "headers": {
+		        "Content-Type": "application/json"
+		    }
+		};
+
+
+		var body = {
+		    "provider": "username",
+		    "data": {
+		        "username": username,
+		        "password": password
+		    }
+		};
+
+		requestOptions.body = JSON.stringify(body);
+		requestOptions.payload = JSON.stringify(body);
+
+
+		fetchAction.fetchUrl(url, requestOptions, function(error, meta, body){
+		    var myObj = JSON.parse(body);
+	
+
+		    if(error){
+		    	console.log('Request Failed:' +error);
+		    }
+		 
+		    	res.status(meta.status).send(meta);
+
+
+		    /*	if(meta.status === 200){
+					 authToken = myObj.auth_token
+			    	 hasura_id = myObj.hasura_id;
+			    	
+			    	var url = "https://data.adulthood94.hasura-app.io/v1/query";
+
+			
+			    console.log(authToken);
+			    
+				var requestOptions = {
+				    "method": "POST",
+				    "headers": {
+				        "Content-Type": "application/json",
+				        "Authorization": "Bearer " + authToken
+				    }
+				};
+
+			var body = {
+			    "type": "insert",
+			    "args": {
+			        "table": "user",
+			        "objects": [
+			            {
+			                "hasura_id": hasura_id,
+			                "firstname": firstname,
+			                "lastname": lastname,
+			                "gender": gender,
+			                "dob": dob,
+			                "username": username,
+			                "time": timeStampInMs
+			            }
+			        ]
+			    }
+			};
+
+			requestOptions.body = JSON.stringify(body);
+			requestOptions.payload = JSON.stringify(body);
+
+			fetchAction.fetchUrl(url, requestOptions,function(error,meta,body){
+		    var myObj = JSON.parse(body);
+
+		 	if(error){
+		    	console.log('Request Failed:' +error);
+		    }
+		    else
+		    {
+		    	console.log(myObj);
+		    
+		    }
+		    
+		 });
+				
+     }*/
+
+	});
+
+	});
+
+
+		app.post('/loginpoint', function(req, res){
+
+
+		var username = req.body.email;
+		var password = req.body.password;
+
+
+		var url = "https://auth.adulthood94.hasura-app.io/v1/login";
+
+		var requestOptions = {
+		    "method": "POST",
+		    "headers": {
+		        "Content-Type": "application/json"
+		    }
+		};
+
+		
+		var body = {
+		    "provider": "username",
+		    "data": {
+		        "username": username,
+		        "password": password
+		    }
+		};
+
+		requestOptions.body = JSON.stringify(body);
+		requestOptions.payload = JSON.stringify(body);
+
+
+		fetchAction.fetchUrl(url, requestOptions, function(error, meta, body){
+		    var myObj = JSON.parse(body);
+		  
+		    if(error){
+		    	console.log('Request Failed:' +error);
+		    }
+		    else
+		    {
+		    	res.send(meta);
+			    var authToken = myObj.auth_token;
+			   
+		    }
+		   
+		});
+
+
+		});
+
+
+
+		app.post('/submitpost',function(req,res){
+
+		
+		var authToken = req.body.authToken;
+		var hasura_id = req.body.hasura_id;	
+
+		var url = "https://data.adulthood94.hasura-app.io/v1/query";
+
+		var requestOptions = {
+		    "method": "POST",
+		    "headers": {
+		        "Content-Type": "application/json",
+		        "Authorization": "Bearer "+ authToken
+		    }
+		};
+
+		var body = {
+		    "type": "insert",
+		    "args": {
+		        "table": "posts",
+		        "objects": [
+		            {
+		                "hasura_id": hasura_id,
+		                "text": text
+		            }
+		        ]
+		    }
+		};
+
+		requestOptions.body = JSON.stringify(body);
+
+		fetchAction.fetchAction(url, requestOptions,function(error,meta,body){
+  			
+  			var myObj = JSON.parse(body);
+		  
+		    if(error){
+		    	console.log('Request Failed:' + error);
+		    }
+		    else
+		    {
+		    	res.status(meta.status).send(body);
+			    
+		    }
+
+
+		});
+		
+		});
+
+
+		app.post('/seeposts',function(req,res){
+
+
+			var authToken = req.body.authToken;
+
+
+		var url = "https://data.adulthood94.hasura-app.io/v1/query";
+
+	
+		var requestOptions = {
+		    "method": "POST",
+		    "headers": {
+		        "Content-Type": "application/json",
+		        "Authorization": "Bearer "+authToken
+		    }
+		};
+
+		var body = {
+		    "type": "select",
+		    "args": {
+		        "table": "posts",
+		        "columns": [
+		            "*"
+		        ]
+		    }
+		};
+
+		requestOptions.body = JSON.stringify(body);
+
+		fetchAction.fetchAction(url, function(error,meta,body){
+			var myObj = JSON.parse(body);
+			if(error){
+		    	console.log('Request Failed:' + error);
+		    }
+		    else
+		    {
+		    	res.status(meta.status).send(myObj);
+			    
+		    }
+		});
 });
+
+
+
+
+		app.listen(8081, function(){
+			console.log('listening on port 8081!');
+		});
+
+
